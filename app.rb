57c33c398c2788af1b1df52c7fb0a27c23eec2bb
Christopher Walsh <walshification@gmail.com>
2016-11-15 22:50:26 -0600

    migrates the db

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
-require 'yaml'
-
-Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each { |file| require file }
-Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each { |file| require file }
-
+# Create the database context
 db_config_file = File.join(File.dirname(__FILE__), "app", "database.yml")
 if File.exist?(db_config_file)
   config = YAML.load(File.read(db_config_file))
   DB = Sequel.connect(config)
+  Sequel.extension :migration
 end
 
+# Connect all our framework's classes
+Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each {|file| require file }
+
+# Connect all our framework's files
+Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each {|file| require file }
+
+# If there is a database connection, run all the migrations
+if DB
+  Sequel::Migrator.run(DB, File.join(File.dirname(__FILE__), 'app', 'db', 'migrations'))
+end
+
+require 'yaml'
 
 ROUTES = YAML.load(File.read(File.join(File.dirname(__FILE__), "app", "routes.yml")))
 
