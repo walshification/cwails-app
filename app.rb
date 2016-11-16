fc284388f304f17ce4823287aa471abe970f62c9
Christopher Walsh <walshification@gmail.com>
2016-11-15 22:35:09 -0600

    adds model architecture and a db

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
+require 'yaml'
+
 Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each { |file| require file }
 Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each { |file| require file }
 
-require 'yaml'
+db_config_file = File.join(File.dirname(__FILE__), "app", "database.yml")
+if File.exist?(db_config_file)
+  config = YAML.load(File.read(db_config_file))
+  DB = Sequel.connect(config)
+end
+
 
 ROUTES = YAML.load(File.read(File.join(File.dirname(__FILE__), "app", "routes.yml")))
 
